af50b167f18d102e8fa9866660a15d7386ee0331
PixelEggplant <pixeleggplant@gmail.com>
2024-02-01 11:29:22 +0900

    Add extra config options for recalculating Wait Time
    Added 2 new options for recalculating the Wait Time. This allows
    the user to specificy whether the Random Timer will recalculate the
    Wait Time on `Timeout` or on `start()`. Essentially, this allows
    the user to have the timer calculate a random Wait Time once and then
    keep using that value moving forward.

--- addons/random_timer_plugin/random_timer.gd
+++ addons/random_timer_plugin/random_timer.gd
@@ -10,8 +10,13 @@ const MIN_WAIT_TIME: float = 0.05
 ## [code]min[/code] is calculated as [code]wait_time - variance[/code].[br]
 ## [code]min[/code] is calculated as [code]wait_time + variance[/code].[br][br]
 ## For example a [b]Wait Time[/b] of [b]3[/b] and a [b]Variance[/b] of [b]1[/b] would produce countdown times between [b]2[/b] and [b]4[/b].[br][br]
-## [b]Note:[/b] The [b]Wait Time[/b] value will never be set lower than [code]0.05[/code].
+## [b]Note:[/b] The [b]Wait Time[/b] value calculated with [b]Variance[/b] will never be set lower than [code]0.05[/code].
 @export_range (0, 4096) var variance: float = 1
+@export_group("Wait Time Recalculation")
+## If [code]true[/code], a new [b]Wait Time[/b] will be calculated every time the [Timer] times out.
+@export var randomize_on_timeout: bool = true
+## If [code]true[/code], a new [b]Wait Time[/b] will be calculated every time the [method RandomTimer.start] is exectuted.
+@export var randomize_on_start: bool = true
 
 var _base_wait_time: float = 0
 
@@ -30,16 +35,20 @@ func _exit_tree() -> void:
 
 
 func start(time_sec: float = -1) -> void:
+	if !randomize_on_start:
+		super(time_sec)
+		return
+
 	if time_sec > 0:
 		_base_wait_time = wait_time
 
 	_randomise_wait_time()
-
 	super()
 
 
 func _on_timeout() -> void:
-	_randomise_wait_time()
+	if randomize_on_timeout:
+		_randomise_wait_time()
 
 
 func _randomise_wait_time() -> void:
